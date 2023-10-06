884872f6ae5fc41477c73b59fdf462f6bf2a4631
nithinK-142 <nithin.sagar.359@gmail.com>
2023-10-06 23:25:49 +0530

    fix: topbar underline.

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { icons } from "../data/icons"
 
 const Topbar = () => {
   return (
     <header className="text-center">
       <div className="bg-[#c2c2af] pt-3 pb-2 text-sm leading-5 underline-offset-2 ">
         Trained Aesop consultants are available to provide bespoke skin care
-        advice.
-        <span className="">
-          <a className="hover:underline font-semibold" href="#">
-            {" "}
+        advice.{" "}
+        <span>
+          <a className="decoration-1 hover:underline font-semibold" href="#">
             Book a video consultation
           </a>
         </span>
@@ -17,9 +15,9 @@ const Topbar = () => {
 
       <div className="text-white bg-[#252525] pt-3 pb-2 text-sm hover:underline underline-offset-[6px] transition duration-1000 ease-in-out cursor-pointer">
         Click and Collect is now available in Hong Kong. Enjoy complimentary
-        shipping on orders over $400.
-        <span className="pl-4">+</span>
+        shipping on orders over $400.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+
       </div>
+
     </header>
   );
 };
