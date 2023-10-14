afbcdcff49da616f784bfe3e2b6601b4431fab51
nithinK-142 <nithin.sagar.359@gmail.com>
2023-10-14 18:36:42 +0530

    feat: footer.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import { footerData } from "../data/footer";
 
 const Footer = () => {
+  const {
+    subscribeText,
+    privacyPolicyLink,
+    normalDivs,
+    sustainabilityDiv,
+    aboutDiv,
+    socialMediaDiv,
+  } = footerData;
   return (
-    <footer className='min-h-[37vh] relative'></footer>
-  )
-}
+    <footer>
+      <div id="footer">
+        <div className="footer-div-one for-lg">
+          <div>
+            <input placeholder="Email address" />
+            <i className="fas fa-arrow-right"></i>
+          </div>
+          <div>
+            <aside></aside>
+            <div>
+              <p>{subscribeText}</p>
+              <h6>{privacyPolicyLink}</h6>
+            </div>
+          </div>
+        </div>
 
-export default Footer
+        <section className="footer-section">
+          <div className="footer-div-one for-sm">
+            <div>
+              <input placeholder="Email address" />
+              <i className="fas fa-arrow-right"></i>
+            </div>
+            <div>
+              <aside></aside>
+              <div>
+                <p>{subscribeText}</p>
+                <h6>{privacyPolicyLink}</h6>
+              </div>
+            </div>
+          </div>
+          {normalDivs.map((div, index) => (
+            <div className="footer-normal-div" key={index}>
+              <h5>{div.title}</h5>
+              {div.links.map((link, linkIndex) => (
+                <p key={linkIndex}>{link}</p>
+              ))}
+            </div>
+          ))}
+        </section>
+        <section className="footer-section">
+          <div className="footer-normal-div">
+            <h5>{sustainabilityDiv.title}</h5>
+            <p>{sustainabilityDiv.text}</p>
+          </div>
+          <div className="footer-normal-div">
+            <h5>{aboutDiv.title}</h5>
+            {aboutDiv.links.map((link, linkIndex) => (
+              <p key={linkIndex}>{link}</p>
+            ))}
+          </div>
+          <div className="footer-normal-div">
+            <h5>{socialMediaDiv.title}</h5>
+            {socialMediaDiv.links.map((link, linkIndex) => (
+              <p key={linkIndex}>{link}</p>
+            ))}
+          </div>
+        </section>
+      </div>
+      <div id="footer-bottom-most">
+        <p>© Aesop</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/data/footer.ts
@@ -0,0 +1,49 @@
+export const footerData = {
+  subscribeText:
+    "Subscribe to receive communications from Aesop about our products and services. By subscribing, you confirm you have read and accept our",
+  privacyPolicyLink: "privacy policy&#8599;",
+  normalDivs: [
+    {
+      title: "Orders and support",
+      links: [
+        "Contact us &#8599;",
+        "FAQs &#8599;",
+        "Shipping &#8599;",
+        "Returns &#8599;",
+        "Terms and conditions",
+      ],
+    },
+    {
+      title: "Services",
+      links: ["Live assistance", "Corporate gifts", "Facial appointments"],
+    },
+    {
+      title: "Location preferences",
+      links: ["Shipping: Hong Kong (S.A.R)", "Language: English"],
+    },
+  ],
+  sustainabilityDiv: {
+    title: "Sustainability",
+    text: "All Aesop products are vegan, and we do not test our formulations or ingredients on animals. We are Leaping Bunny approved and a Certified B Corporation. Learn more",
+  },
+  aboutDiv: {
+    title: "About",
+    links: [
+      "Our story",
+      "Foundation &#8599;",
+      "Careers",
+      "Privacy policy",
+      "Accessibility",
+    ],
+  },
+  socialMediaDiv: {
+    title: "Social media",
+    links: [
+      "Instagram &#8599;",
+      "Twitter &#8599;",
+      "LinkedIn &#8599;",
+      "WeChat",
+      "Weibo &#8599;",
+    ],
+  },
+};
